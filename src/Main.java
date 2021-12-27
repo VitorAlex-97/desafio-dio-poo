53cdabe145bc6cc2b8de86ef62a2e9d1adc6b259
VitorAlex-97 <vitualex97@gmail.com>
2021-12-27 09:59:39 -0300

    Mentoria e Curso criadas

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import br.com.dio.desafio.Curso;
+import br.com.dio.desafio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Abstraindo curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso1.setTitulo("Curso JavaScript");
+        curso1.setDescricao("Descrição JavaScipt");
+        curso1.setCargaHoraria(6);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+        System.out.println(curso1);
+        System.out.println(curso2);
+    }
+}
