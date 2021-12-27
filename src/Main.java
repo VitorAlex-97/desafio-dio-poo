efcf23fe6d7b84f12238fc2b9e6bd87a098350fc
VitorAlex-97 <vitualex97@gmail.com>
2021-12-27 12:14:55 -0300

    Commit final: Integrando as Classes de domínio

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.Bootcamp;
 import br.com.dio.desafio.Curso;
+import br.com.dio.desafio.Dev;
 import br.com.dio.desafio.Mentoria;
 
 import java.time.LocalDate;
@@ -16,13 +18,41 @@ public class Main {
         curso2.setCargaHoraria(6);
 
         Mentoria mentoria = new Mentoria();
-
         mentoria.setTitulo("Mentoria Java");
         mentoria.setDescricao("Descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(mentoria);
-        System.out.println(curso1);
-        System.out.println(curso2);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devVitor = new Dev();
+        devVitor.setNome("Vitor");
+        devVitor.InscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Vitor: "+ devVitor.getContedosInscritos());
+        devVitor.progredir();
+        devVitor.progredir();
+        devVitor.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos concluidos Vitor: "+ devVitor.getConteudosConcluidos());
+        System.out.println("Conteúdos inscritos Vitor: "+ devVitor.getContedosInscritos());
+        System.out.println("XP: " + devVitor.calcularTotalXp());
+
+        System.out.println("--------------------------------");
+
+        Dev devLucas = new Dev();
+        devLucas.setNome("Lucas");
+        devLucas.InscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Lucas: "+ devLucas.getContedosInscritos());
+        devLucas.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos concluidos Lucas: "+ devLucas.getConteudosConcluidos());
+        System.out.println("Conteúdos inscritos Lucas: "+ devLucas.getContedosInscritos());
+        devLucas.progredir();
+        System.out.println("XP: " + devLucas.calcularTotalXp());
+
     }
 }
